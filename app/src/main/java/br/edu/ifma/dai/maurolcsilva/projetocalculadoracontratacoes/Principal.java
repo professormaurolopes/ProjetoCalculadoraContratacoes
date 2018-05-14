89dc7e7ba68c5e90434f24e4b0c479f645e3c307
Mauro Silva <maurosilva@ifma.edu.br>
2018-05-14 17:38:36 -0300

    Atualização com a utilização dos CheckBoxs

--- app/src/main/java/br/edu/ifma/dai/maurolcsilva/projetocalculadoracontratacoes/Principal.java
+++ app/src/main/java/br/edu/ifma/dai/maurolcsilva/projetocalculadoracontratacoes/Principal.java
@@ -3,7 +3,9 @@ package br.edu.ifma.dai.maurolcsilva.projetocalculadoracontratacoes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -16,6 +18,8 @@ public class Principal extends AppCompatActivity {
     private Spinner spnMeses;
     private RadioGroup rdgExAluno;
     private Button btnCalcular;
+    private CheckBox chk01,chk02,chk03;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,13 +28,25 @@ public class Principal extends AppCompatActivity {
         txtNomeContratado = findViewById(R.id.txtNomeContratado);
         txtSalario = findViewById(R.id.txtSalarioContratado);
         spnMeses = findViewById(R.id.spnMesdeEntrada);
+        chk01 = findViewById(R.id.chkOpcao01);
+        chk02 = findViewById(R.id.chkOpcao02);
+        chk03 = findViewById(R.id.chkOpcao03);
+        //Criação dos dados da caixa de selecao
+        String meses[] = {"Janeiro","Fevereiro","Março"};
+        //Craição do ArrayAdapter
+        ArrayAdapter<String> meuadapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,meses);
+        //Associar o Adapter ao Spinner
+        spnMeses.setAdapter(meuadapter);
         rdgExAluno = findViewById(R.id.rdgExAluno);
     }
 
     public void calcularSalario(View v){
         int opcao;
-        String msg="";
+        String msg="",mes="",nomeContratado="",resultadoselecao="";
 
+        nomeContratado = txtNomeContratado.getText().toString();
+        //Recupera o que foi selecionado no Spinner
+        mes = spnMeses.getSelectedItem().toString();
         opcao = rdgExAluno.getCheckedRadioButtonId();
         switch (opcao){
             case R.id.rdSim:
@@ -42,6 +58,16 @@ public class Principal extends AppCompatActivity {
                 msg = "Não é Ex-Aluno";
                 break;
         }
+        if (chk01.isChecked()){
+            resultadoselecao = chk01.getText().toString();
+        }
+        if (chk02.isChecked()){
+            resultadoselecao = resultadoselecao + " " + chk02.getText().toString();
+        }
+        if (chk03.isChecked()){
+            resultadoselecao = resultadoselecao + " " + chk03.getText().toString();
+        }
+        msg = msg + " " + mes + " " + resultadoselecao;
         Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
 
     }
